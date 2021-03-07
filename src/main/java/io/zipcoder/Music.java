ca846cd454a139ca10ba6835033126b6740a866e
mike <mike@zipcodes-mbp-5.lan>
2021-03-07 18:43:25 -0500

    completed playlist selection

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -4,11 +4,37 @@ public class Music {
 
     private String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+
+    int nextSong = 0;
+    int prevSong = 0;
+    int currIndex = 0;
+
+
+    for (int i = 0; i < playList.length; i++){
+        if (playList[(startIndex + i) % playList.length] == selection) {
+            break;
+        }
+        nextSong++;
+
+        if (playList[(((startIndex - i) % playList.length)+ playList.length) % playList.length] == selection){
+            break;
+        }
+        prevSong++;
+
+        System.out.println("Next song count" + nextSong);
+        System.out.println("Prev song count" + prevSong);
+
     }
+        if (nextSong < prevSong) {
+            return nextSong;
+        }else{
+            return prevSong;
+        }
+    }
+
 }
